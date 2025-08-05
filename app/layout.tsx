23601f42022719c89c469bfbe83e6ce0614da1c6
hjkim <20220330jin@gmail.com>
2025-08-05 16:42:39 +0900

    correct lint error

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,8 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   title: "오늘 뭐 먹지? - lunchyo",
-  description: "점심 메뉴 추천부터 주변 맛집 탐색까지, 당신의 점심 고민을 해결해 드립니다.",
+  description:
+    "점심 메뉴 추천부터 주변 맛집 탐색까지, 당신의 점심 고민을 해결해 드립니다.",
 };
 
 export default function RootLayout({
@@ -13,9 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
